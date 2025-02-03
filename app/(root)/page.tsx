7e9ce73f1dcdae17dbe587cdc25c86a8e2b7b10c
Doğukan Tekin <dogukantekin.ce@gmail.com>
2025-02-03 18:00:18 +0300

    right sidebar added, layout completed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,11 @@
 import HeaderBox from '@/components/HeaderBox';
+import RightSideBar from '@/components/RightSideBar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import { log } from 'console';
 import React from 'react';
 
 const Home = () => {
-    const loggedIn = { firstName: 'Doğukan' };
+    const loggedIn = { firstName: 'Doğukan', lastName: 'Tekin', email: 'dogukantekin.ce@gmail.com' };
 
     return (
         <section className='home'>
@@ -22,7 +24,15 @@ const Home = () => {
                         totalCurrentBalance={1250.35}
                     />
                 </header>
+
+                RECENT TRANSACTION
             </div>
+
+            <RightSideBar 
+                user={loggedIn}
+                transactions={[]}
+                banks={[{ currentBalance: 123.50 }, { currentBalance: 500.50 }]}
+            />
         </section>
     )
 }
